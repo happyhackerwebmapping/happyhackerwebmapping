c89ab0e02e8f0d4892ba516b4957cb79a0694a4a
unknown <robin.kohrs@gmx.de>
2019-05-30 11:34:55 +0200

    add lightpollutionmap

--- licht.js
+++ licht.js
@@ -1,65 +1,29 @@
-let karte = L.map("map");
+let karte = L.map(`map`, {
+    center: [47, 9],
+    zoom: 3
+});
 
-const kartenLayer = {
-    osm: L.tileLayer("https://{s}.tile.osm.org/{z}/{x}/{y}.png", {
-        subdomains: ["a", "b", "c"],
+var kartenLayer = {
+    VIIRS_2019: L.tileLayer.wms("https://www.lightpollutionmap.info/geoserver/gwc/service/wms?", {
+        layers: 'PostGIS:VIIRS_2019',
+        transparent: true,
+        opacity: 0.5,
+    }),
+
+    VIIRS_2014: L.tileLayer.wms("https://www.lightpollutionmap.info/geoserver/gwc/service/wms?", {
+        layers: 'PostGIS:VIIRS_2014',
+        transparent: true,
+    }),
+    osm: L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png", { //{} --> steht für Objekt   [] --> array,   //{s} kann Server für 
+        subdomains: ["a", "b", "c"], //Server
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>'
     }),
-    geolandbasemap: L.tileLayer("https://{s}.wien.gv.at/basemap/geolandbasemap/normal/google3857/{z}/{y}/{x}.png", {
-        subdomains: ["maps", "maps1", "maps2", "maps3", "maps4"],
-        attribution: 'Datenquelle: <a href="https://www.basemap.at">basemap.at</a>'
-    }),
-    bmapoverlay: L.tileLayer("https://{s}.wien.gv.at/basemap/bmapoverlay/normal/google3857/{z}/{y}/{x}.png", {
-        subdomains: ["maps", "maps1", "maps2", "maps3", "maps4"],
-        attribution: 'Datenquelle: <a href="https://www.basemap.at">basemap.at</a>'
-    }),
-    bmapgrau: L.tileLayer("https://{s}.wien.gv.at/basemap/bmapgrau/normal/google3857/{z}/{y}/{x}.png", {
-        subdomains: ["maps", "maps1", "maps2", "maps3", "maps4"],
-        attribution: 'Datenquelle: <a href="https://www.basemap.at">basemap.at</a>'
-    }),
-    bmaphidpi: L.tileLayer("https://{s}.wien.gv.at/basemap/bmaphidpi/normal/google3857/{z}/{y}/{x}.jpeg", {
-        subdomains: ["maps", "maps1", "maps2", "maps3", "maps4"],
-        attribution: 'Datenquelle: <a href="https://www.basemap.at">basemap.at</a>'
-    }),
-    bmaporthofoto30cm: L.tileLayer("https://{s}.wien.gv.at/basemap/bmaporthofoto30cm/normal/google3857/{z}/{y}/{x}.jpeg", {
-        subdomains: ["maps", "maps1", "maps2", "maps3", "maps4"],
-        attribution: 'Datenquelle: <a href="https://www.basemap.at">basemap.at</a>'
-    }),
-    bmapgelaende: L.tileLayer("https://{s}.wien.gv.at/basemap/bmapgelaende/grau/google3857/{z}/{y}/{x}.jpeg", {
-        subdomains: ["maps", "maps1", "maps2", "maps3", "maps4"],
-        attribution: 'Datenquelle: <a href="https://www.basemap.at">basemap.at</a>'
-    }),
-    bmapoberflaeche: L.tileLayer("https://{s}.wien.gv.at/basemap/bmapoberflaeche/grau/google3857/{z}/{y}/{x}.jpeg", {
-        subdomains: ["maps", "maps1", "maps2", "maps3", "maps4"],
-        attribution: 'Datenquelle: <a href="https://www.basemap.at">basemap.at</a>'
-    }),
-    stamen_toner: L.tileLayer("https://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}.png", {
-        subdomains: ["a", "b", "c"],
-        attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://www.openstreetmap.org/copyright">ODbL</a>.'
-    }),
-    stamen_terrain: L.tileLayer("https://stamen-tiles-{s}.a.ssl.fastly.net/terrain/{z}/{x}/{y}.jpg", {
-        subdomains: ["a", "b", "c"],
-        attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://www.openstreetmap.org/copyright">ODbL</a>.'
-    }),
-    stamen_watercolor: L.tileLayer("https://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.jpg", {
-        subdomains: ["a", "b", "c"],
-        attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://creativecommons.org/licenses/by-sa/3.0">CC BY SA</a>.'
-    })
 };
 
-const layerControl = L.control.layers({
-    "Geoland Basemap": kartenLayer.geolandbasemap,
-    "Geoland Basemap Grau": kartenLayer.bmapgrau,
-    "Geoland Basemap Overlay": kartenLayer.bmapoverlay,
-    "Geoland Basemap High DPI": kartenLayer.bmaphidpi,
-    "Geoland Basemap Orthofoto": kartenLayer.bmaporthofoto30cm,
-    "Geoland Basemap Gelände": kartenLayer.bmapgelaende,
-    "Geoland Basemap Oberfläche": kartenLayer.bmapoberflaeche,
-    "OpenStreetMap": kartenLayer.osm,
-    "Stamen Toner": kartenLayer.stamen_toner,
-    "Stamen Terrain": kartenLayer.stamen_terrain,
-    "Stamen Watercolor": kartenLayer.stamen_watercolor
+L.control.layers({
+    "VIIRS_2019": kartenLayer.VIIRS_2019,
+    "osm": kartenLayer.osm,
 }).addTo(karte);
 
-kartenLayer.bmapgrau.addTo(karte);
-karte.addControl(new L.Control.Fullscreen());
+kartenlayer.osm.addTo(karte);
+kartenlayer.VIIRS_2019.addTo(karte);
