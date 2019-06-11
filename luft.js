f5c7fc14425541f2ee7ad5bc11b9922aa7ae6b36
Höfner <csav9726@uibk.ac.at>
2019-06-11 21:03:06 +0200

    luft heatmap

--- luft.js
+++ luft.js
@@ -1,5 +1,5 @@
 let karte = L.map("map", {
-    
+
 });
 karte.setView(
     [48, 13], 7
@@ -17,7 +17,7 @@ var kartenLayer = {
         subdomains: ["maps", "maps1", "maps2", "maps3", "maps4"],
         attribution: 'Datenquelle: <a href="https://www.basemap.at">basemap.at</a>'
     }),
-        bmpgrau: L.tileLayer("https://{s}.wien.gv.at/basemap/bmapgrau/normal/google3857/{z}/{y}/{x}.png", {
+    bmpgrau: L.tileLayer("https://{s}.wien.gv.at/basemap/bmapgrau/normal/google3857/{z}/{y}/{x}.png", {
         subdomains: ["maps", "maps1", "maps2", "maps3", "maps4"],
         attribution: 'Datenquelle: <a href="https://www.basemap.at">basemap.at</a>'
     }),
@@ -34,27 +34,28 @@ L.control.layers({
 }).addTo(karte);
 
 // Jsonp abfragen
-let jsonpResponse = function(data) {
+let jsonpResponse = function (data) {
     // console.log(data);
-    for (let i in data){
+    for (let i in data) {
         // console.log(data[i].lat);
         let lat = data[i].lat
         let lng = data[i].lon
-        let ozon1h = data[i].ozon1h 
+        let ozon1h = data[i].ozon1h
         let ozon1hmax = data[i].ozon1hMax
         let name = data[i].name
         let timestamp = data[i].timestamp_utc
-        
+
         // Marker
-        let ozonMarker = L.marker([lat,lng], { 
-        }).addTo(karte)
+        let ozonMarker = L.marker([lat, lng], {}).addTo(karte)
         ozonMarker.bindPopup(`
         Name: ${name}
-        Timestamp: ${timestamp}`)
+        Timestamp: ${timestamp}<br>
+        Ozon 1h Max: ${ozon1hmax}
+        `)
 
         var ozonPoints = [lat, lng, ozon1hmax]
         console.log(ozonPoints)
-        
+
         // var idw = L.idwLayer([ozonPoints],{
         //     opacity: 0.3,
         //     maxZoom: 18,
@@ -64,12 +65,43 @@ let jsonpResponse = function(data) {
         // }).addTo(karte);
 
 
-        
+        // var cfg = {
+        //     // radius should be small ONLY if scaleRadius is true (or small radius is intended)
+        //     // if scaleRadius is false it will be the constant radius used in pixels
+        //     "radius": 2,
+        //     "maxOpacity": .8, 
+        //     // scales the radius based on map zoom
+        //     "scaleRadius": true, 
+        //     // if set to false the heatmap uses the global maximum for colorization
+        //     // if activated: uses the data maximum within the current map boundaries 
+        //     //   (there will always be a red spot with useLocalExtremas true)
+        //     "useLocalExtrema": true,
+        //     // which field name in your data represents the latitude - default "lat"
+        //     latField: 'lat',
+        //     // which field name in your data represents the longitude - default "lng"
+        //     lngField: 'lng',
+        //     // which field name in your data represents the data value - default "value"
+        //     valueField: 'count'
+        //   };
+        //   var heatmapLayer = new HeatmapOverlay(cfg);
+
+
+
+        //   heatmapLayer.setData(ozonPoints);
+
+        var heat = L.heatLayer([
+                ozonPoints
+            ], // lat, lng, intensity
+
+            {
+                radius: 25,
+                
+            }).addTo(karte);
     }
 
-    
-    
-    
+
+
+
 
 }
 
