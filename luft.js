95e322e7b3958a956f5f98917dcd2d58f43c870b
Höfner <csav9726@uibk.ac.at>
2019-06-14 10:40:57 +0200

    marker

--- luft.js
+++ luft.js
@@ -27,75 +27,102 @@ var kartenLayer = {
 
 kartenLayer.bmpgrau.addTo(karte);
 
-L.control.layers({
+const layerControl = L.control.layers({
     "Basemap Grau": kartenLayer.bmpgrau,
     "Geoland Basemap": kartenLayer.geolandbasemap,
     "OSM": kartenLayer.osm,
 }).addTo(karte);
+
+const heatgruppe = L.featureGroup();
+const markergruppe = L.featureGroup();
 
 // Jsonp abfragen
 let jsonpResponse = function (data) {
     // console.log(data);
     for (let i in data) {
         // console.log(data[i].lat);
-        let lat = data[i].lat
-        let lng = data[i].lon
-        let ozon1h = data[i].ozon1h
-        let ozon1hmax = data[i].ozon1hMax
-        let name = data[i].name
-        let timestamp = data[i].timestamp_utc
+        let lat = data[i].lat;
+        let lng = data[i].lon;
+        let ozon1h = data[i].ozon1h;
+        let ozon8h = data[i].ozon8h;
+        let ozon1hmax = data[i].ozon1hMax;
+        let name = data[i].name;
+        let timestamp = data[i].timestamp_utc;
+
+
+        //Thu, 13 Jun 2019 23:00:00 GMT
+        //Zeitzone entfernen
+        let date = timestamp.substring(0, timestamp.length-12);
+        //Uhrzeit auslesen
+        let time = timestamp.substring(timestamp.length-12, timestamp.length-4);
+        
+        
 
         // Marker
-        let ozonMarker = L.marker([lat, lng], {}).addTo(karte)
-        ozonMarker.bindPopup(`
-        Name: ${name}
-        Timestamp: ${timestamp}<br>
-        Ozon 1h Max: ${ozon1hmax}
-        `)
 
-        var ozonPoints = [lat, lng, ozon1hmax/300]
+        // const farbpalette_marker = [
+        //     [0,"#41ab5d"],
+        //     [30,"#d9f0a3"],
+        //     [60,"#ffffcc"],
+        //     [90,"#ffeda0"],
+        //     [120,"#fed976"],
+        //     [150,"#feb24c"],
+        //     [180,"#fd8d3c"],
+        //     [210,"#fc4e2a"],
+        //     [240,"#e31a1c"],
+        //     [270,"#bd0026"],
+        //     [300,"#800026"],
+        // ]
+
+        // if (ozon1h) {
+        //     let color = "green";
+        //     for (let i =0; i <farbpalette_marker.length; i++) {
+        //         if (ozon1h < farbpalette_marker[i][0]) {
+        //             color = farbpalette_marker[i][1];
+        //             break
+        //         }
+        //     }
+        //     return L.marker(lat, lng, {
+        //         icon: L.divIcon({
+        //             html: `style ="background-color: ${color}">${ozon1h}`
+        //         })
+        //     }).addTo(markergruppe)
+        // }
+
+        let ozonMarker = L.marker([lat, lng], {}).addTo(karte)
+        
+        ozonMarker.bindPopup(`
+        <h4 class = "marker" >Name: ${name}</h4>
+        
+        <p class = "marker" >
+        <strong>Datum:</strong> ${date}<br>
+        <strong>Uhrzeit:</strong> ${time}<br>
+        <strong>Ozon 1h max:</strong> ${ozon1hmax}<br>
+        <strong>Ozon 8h:</strong> ${ozon8h}</p>
+        `).addTo(markergruppe);
+
+        markergruppe.addTo(karte)
+       
+        
+        // Heatmap
+
+        var ozonPoints = [lat, lng, parseFloat(ozon1h/300)]
         console.log(ozonPoints)
 
-        // var idw = L.idwLayer([ozonPoints],{
-        //     opacity: 0.3,
-        //     maxZoom: 18,
-        //     cellSize: 10,
-        //     exp: 3,
-        //     max: 50
-        // }).addTo(karte);
+     
 
-
-        // var cfg = {
-        //     // radius should be small ONLY if scaleRadius is true (or small radius is intended)
-        //     // if scaleRadius is false it will be the constant radius used in pixels
-        //     "radius": 2,
-        //     "maxOpacity": .8, 
-        //     // scales the radius based on map zoom
-        //     "scaleRadius": true, 
-        //     // if set to false the heatmap uses the global maximum for colorization
-        //     // if activated: uses the data maximum within the current map boundaries 
-        //     //   (there will always be a red spot with useLocalExtremas true)
-        //     "useLocalExtrema": true,
-        //     // which field name in your data represents the latitude - default "lat"
-        //     latField: 'lat',
-        //     // which field name in your data represents the longitude - default "lng"
-        //     lngField: 'lng',
-        //     // which field name in your data represents the data value - default "value"
-        //     valueField: 'count'
-        //   };
-        //   var heatmapLayer = new HeatmapOverlay(cfg);
-
-
-
-        //   heatmapLayer.setData(ozonPoints);
-
-        var heat = L.heatLayer([
+        L.heatLayer([
                 ozonPoints
             ], // lat, lng, intensity
 
             {
-                radius: 50,
-                opacity: 0.5,
+                radius: 25,
+                minOpacity: 0.4,
+                
+                
+                
+                
+                
                 
                 //Grenzwerte
                 //300µg: 1
@@ -111,17 +138,22 @@ let jsonpResponse = function (data) {
 
 
                 gradient: {   
-                    0.1: "green",
+                    0.0: "blue",
+                    0.1: "black",
                     0.2: "yellow",
                     0.3: "orange",
                     0.4: "red",
+                    0.5: "purple",
                     
                     
 
                 }
-            }).addTo(karte);
+            }).addTo(heatgruppe);
     }
-
+    
+    layerControl.addOverlay(markergruppe, "Ozonmessstationen")
+    layerControl.addOverlay(heatgruppe, "Heatmap")
+    
 
 
 
@@ -131,15 +163,9 @@ let jsonpResponse = function (data) {
 
 
 // // Ozonwerte
-// const url = "http://luft.umweltbundesamt.at/pub/ozonbericht/aktuell.json";
+//http://luft.umweltbundesamt.at/pub/ozonbericht/aktuell.json
 
-// async function OzonLaden(url) {
-//     console.log("lade", url);
 
-//     const antwort = await fetch(url);
-//     const jsonDaten = await antwort.json();
-// };
-// OzonLaden();
 
 // Fullscreen
 karte.addControl(new L.Control.Fullscreen());
