a75b97406566d8ba048a3fcf9d1e598589e8b862
alexhoefner <xwerbung@yahoo.de>
2019-06-15 12:21:42 +0200

    luft

--- luft.js
+++ luft.js
@@ -109,7 +109,7 @@ let jsonpResponse = function (data) {
         var ozonPoints = [lat, lng, parseFloat(ozon1h/300)]
         console.log(ozonPoints)
 
-     
+        
 
         L.heatLayer([
                 ozonPoints
@@ -169,3 +169,34 @@ let jsonpResponse = function (data) {
 
 // Fullscreen
 karte.addControl(new L.Control.Fullscreen());
+
+
+//// ImageSlides
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+} 
